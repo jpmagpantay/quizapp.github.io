2749f41e8cee64ecd3f6620d4123dab7373210ed
jpmagpantay <123483825+jpmagpantay@users.noreply.github.com>
2023-01-28 14:24:35 +0100

    changing questions to quiz.length to see how many questions in total

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
  const homeBox = document.querySelector(".home-box");
  const quizBox = document.querySelector(".quiz-box");
  const resultBox = document.querySelector(".result-box");
- const questionLimit = 25; // if you want all questions "quiz.length"
+ const questionLimit = quiz.length; // if you want all questions "quiz.length"
  let questionCounter = 0;
  let currentQuestion;
  let availableQuestions = [];
